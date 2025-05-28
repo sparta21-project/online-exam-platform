49e347270fdbba68d03dd5049dca4b59633c9343
jungyeon <jungnam9742@gmail.com>
2025-05-29 00:39:04 +0900

    project setup

---
+++ src/main/java/com/example/onlineexamplatform/OnlineExamPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlineexamplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineExamPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineExamPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onlineexamplatform/OnlineExamPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineexamplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineExamPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
