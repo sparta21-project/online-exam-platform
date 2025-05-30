01554b5de351bdf9fd49af88f6490ee2a802aac8
HanGil <jhg9240@gmail.com>
2025-05-30 14:39:25 +0900

    refactor: 예외 처리 적용

--- src/main/java/com/example/onlineexamplatform/domain/exam/repository/ExamRepository.java
+++ src/main/java/com/example/onlineexamplatform/domain/exam/repository/ExamRepository.java
@@ -2,12 +2,14 @@ package com.example.onlineexamplatform.domain.exam.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.onlineexamplatform.common.code.ErrorStatus;
+import com.example.onlineexamplatform.common.error.ApiException;
 import com.example.onlineexamplatform.domain.exam.entity.Exam;
 
 public interface ExamRepository extends JpaRepository<Exam, Long> {
 
-	// default Exam findByIdOrElseThrow(Long id) {
-	// 	return findById(id).orElseThrow(()-> new ApiException());
-	// }
+	default Exam findByIdOrElseThrow(Long id) {
+		return findById(id).orElseThrow(() -> new ApiException(ErrorStatus.EXAM_NOT_FOUND));
+	}
 
 }
