006982027f698a08e70ea6263f007b5edbf83861
김민준 <rlaalswns@naver.com>
2025-06-02 16:33:22 +0900

    feat:로그아웃 기능 구현

--- src/main/java/com/example/onlineexamplatform/domain/user/controller/UserController.java
+++ src/main/java/com/example/onlineexamplatform/domain/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.onlineexamplatform.domain.user.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -61,4 +62,16 @@ public class UserController {
 		userService.changePassword(userId, request);
 		return ApiResponse.onSuccess(SuccessStatus.UPDATE_PASSWORD);
 	}
+
+	@DeleteMapping("/logout")
+	public ResponseEntity<ApiResponse<Void>> logout(HttpSession session) {
+		Long userId = (Long)session.getAttribute(SESSION_USER_KEY);
+		if (userId == null) {
+			throw new ApiException(ErrorStatus.UNAUTHORIZED);
+		}
+
+		session.invalidate();
+		return ApiResponse.onSuccess(SuccessStatus.LOGOUT_SUCCESS);
+	}
+
 }
