d3e06ce10b5df7a5e90556d4f9ca2902ff23b682
HanGil <jhg9240@gmail.com>
2025-06-03 09:47:58 +0900

    feat: 페이징 기능 구현

--- src/main/java/com/example/onlineexamplatform/domain/exam/service/ExamService.java
+++ src/main/java/com/example/onlineexamplatform/domain/exam/service/ExamService.java
@@ -1,7 +1,7 @@
 package com.example.onlineexamplatform.domain.exam.service;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,7 +10,7 @@ import com.example.onlineexamplatform.common.error.ApiException;
 import com.example.onlineexamplatform.domain.exam.dto.request.CreateExamRequestDto;
 import com.example.onlineexamplatform.domain.exam.dto.request.UpdateExamRequestDto;
 import com.example.onlineexamplatform.domain.exam.dto.response.ExamResponseDto;
-import com.example.onlineexamplatform.domain.exam.dto.response.GetExamListReponseDto;
+import com.example.onlineexamplatform.domain.exam.dto.response.GetExamListResponseDto;
 import com.example.onlineexamplatform.domain.exam.dto.response.UpdateExamResponseDto;
 import com.example.onlineexamplatform.domain.exam.entity.Exam;
 import com.example.onlineexamplatform.domain.exam.repository.ExamRepository;
@@ -51,19 +51,15 @@ public class ExamService {
 	}
 
 	// TODO 캐시 적용
-	public List<GetExamListReponseDto> getExamList() {
-		return examRepository.findAll()
-			.stream()
-			.map(GetExamListReponseDto::toDto)
-			.toList();
+	public Page<GetExamListResponseDto> getExamList(Pageable pageable) {
+		return examRepository.findAll(pageable)
+			.map(GetExamListResponseDto::toDto);
 	}
 
 	// TODO 레디스 적용 적용
-	public List<GetExamListReponseDto> searchExamByTitle(String examTitle) {
-		return examRepository.findByExamTitle(examTitle)
-			.stream()
-			.map(GetExamListReponseDto::toDto)
-			.toList();
+	public Page<GetExamListResponseDto> searchExamByTitle(Pageable pageable, String examTitle) {
+		return examRepository.findByExamTitle(pageable, examTitle)
+			.map(GetExamListResponseDto::toDto);
 	}
 
 	// TODO 캐시 적용
