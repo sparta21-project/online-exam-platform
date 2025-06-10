b8b5eb583f50234786397a8fd2ccf470c645b902
jungyeon <jungnam9742@gmail.com>
2025-06-11 00:20:34 +0900

    feat: Swagger설정 및 의존성 추가

---
+++ src/main/java/com/example/onlineexamplatform/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.example.onlineexamplatform.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components()) // 공통적으로 사용되는 구성요소 등록 용도 , SecuritySchemes, Schemas, Responses, RequestBodies 등
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Online Exam Platform API") // API 제목
+                .description("온라인 시험 플랫폼 백엔드 API 문서") // API 설명
+                .version("1.0.0"); // API 버전
+    }
+}
