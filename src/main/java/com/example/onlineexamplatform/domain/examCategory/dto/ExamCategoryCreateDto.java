99b9d53d15dd97a9967d9350b9addd983306553a
jungyeon <jungnam9742@gmail.com>
2025-06-11 03:01:33 +0900

    feat: UserAnswer, ExamAnswer, ExamCategory Swagger 적용

--- src/main/java/com/example/onlineexamplatform/domain/examCategory/dto/ExamCategoryCreateDto.java
+++ src/main/java/com/example/onlineexamplatform/domain/examCategory/dto/ExamCategoryCreateDto.java
@@ -1,5 +1,6 @@
 package com.example.onlineexamplatform.domain.examCategory.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
@@ -7,8 +8,10 @@ import lombok.Getter;
 public class ExamCategoryCreateDto {
 
     @NotNull(message = "ID는 필수입니다.")
+    @Schema(description = "시험 ID", example = "1")
     private Long examId;
 
     @NotNull(message = "Category 값은 필수입니다.")
+    @Schema(description = "권한 ID", example = "1")
     private Long categoryId;
 }
