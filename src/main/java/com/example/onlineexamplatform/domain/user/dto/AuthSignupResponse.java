7b33473b17a9201425d6b20aa2a957430cb0c9bd
김민준 <rlaalswns@naver.com>
2025-06-11 19:39:02 +0900

    feat:Swagger 의존성 추가 및 어노테이션 추가 후 프로필수정 코드 리팩터링

--- src/main/java/com/example/onlineexamplatform/domain/user/dto/AuthSignupResponse.java
+++ src/main/java/com/example/onlineexamplatform/domain/user/dto/AuthSignupResponse.java
@@ -1,5 +1,6 @@
 package com.example.onlineexamplatform.domain.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,9 +8,17 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "회원가입 응답 정보")
 public class AuthSignupResponse {
+	@Schema(description = "사용자 ID", example = "1")
 	private Long id;
+
+	@Schema(description = "사용자 이메일", example = "user@example.com")
 	private String email;
+
+	@Schema(description = "사용자 이름", example = "김민준")
 	private String username;
+
+	@Schema(description = "사용자 권한", example = "USER")
 	private String role;
 }
