b8773798f4940ea37b9381c84f9a9dc652d743eb
HanGil <jhg9240@gmail.com>
2025-06-12 17:27:06 +0900

    refactor: 시험파일 DB에 저장하는 로직이 try-catch문 안에서 실행되도록 리팩토링, 직접 시험파일 삭제하는 로직 제거

--- src/main/java/com/example/onlineexamplatform/domain/examFile/service/S3UploadService.java
+++ src/main/java/com/example/onlineexamplatform/domain/examFile/service/S3UploadService.java
@@ -3,11 +3,7 @@ package com.example.onlineexamplatform.domain.examFile.service;
 import static com.example.onlineexamplatform.common.awsS3Util.MimeTypeUtil.*;
 
 import java.io.InputStream;
-import java.net.URI;
-import java.net.URL;
 import java.net.URLConnection;
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
@@ -100,31 +96,31 @@ public class S3UploadService {
 			software.amazon.awssdk.services.s3.model.PutObjectRequest putObjectRequest = software.amazon.awssdk.services.s3.model.PutObjectRequest.builder()
 				.bucket(bucketName) // 버킷 이름
 				.key(s3FileName) // 저장할 파일 이름
-				.contentType(mimeType) // 이미지 MIME 타입
+				.contentType(mimeType) // 시험파일 MIME 타입
 				.contentLength(file.getSize()) // 파일 크기
 				.build();
 
-			// S3에 이미지 업로드
+			// S3에 시험파일 업로드
 			s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(inputStream, file.getSize()));
+			// ExamFile Entity에 업로드된 파일 정보 저장
+			ExamFile examFile = ExamFile.builder()
+				.fileName(file.getOriginalFilename())
+				.path(s3FileName)
+				.fileType(file.getContentType())
+				.size(file.getSize())
+				.build();
+
+			// 시험파일 DB에 저장
+			examFileRepository.save(examFile);
+
+			// path 반환
+			return ExamFileResponseDto.of(examFile);
 
 		} catch (Exception exception) {
 			log.error(exception.getMessage(), exception);
 			throw new ApiException(ErrorStatus.IO_EXCEPTION_UPLOAD_FILE);
 		}
 
-		// ExamFile Entity에 업로드된 파일 정보 저장
-		ExamFile examFile = ExamFile.builder()
-			.fileName(file.getOriginalFilename())
-			.path(s3FileName)
-			.fileType(file.getContentType())
-			.size(file.getSize())
-			.build();
-
-		// 시험파일 DB에 저장
-		examFileRepository.save(examFile);
-
-		// path 반환
-		return ExamFileResponseDto.of(examFile);
 	}
 
 	public List<ExamFile> findAllByImageId(List<Long> examFileIds) {
@@ -133,43 +129,6 @@ public class S3UploadService {
 			throw new ApiException(ErrorStatus.IMAGE_ID_MISSING);
 		return examFiles;
 
-	}
-
-	// 이미지의 path를 이용하여 S3에서 해당 이미지를 제거, getKeyFromImageAddress 메서드를 호출하여 삭제에 필요한 key 획득
-	public void delete(List<String> imagePaths) {
-		List<String> keys = imagePaths.stream()
-			.map(this::getKeyFromImagePaths)
-			.toList();
-
-		try {
-			// S3에서 파일을 삭제하기 위한 요청 객체 생성
-			DeleteObjectsRequest deleteObjectsRequest = DeleteObjectsRequest.builder()
-				.bucket(bucketName) // S3 버킷 이름 지정
-				.delete(delete -> delete.objects(
-					// S3 객체들을 삭제할 객체 목록을 생성
-					keys.stream()
-						.map(key -> ObjectIdentifier.builder().key(key).build())
-						.toList()
-				))
-				.build();
-			s3Client.deleteObjects(deleteObjectsRequest); // S3에서 객체 삭제
-		} catch (Exception exception) {
-			log.error(exception.getMessage(), exception);
-			throw new ApiException(ErrorStatus.IO_EXCEPTION_DELETE_FILE);
-		}
-	}
-
-	// 삭제에 필요한 key 반환
-	private String getKeyFromImagePaths(String imagePath) {
-		try {
-			URL url = new URI(imagePath).toURL(); // 인코딩된 주소를 Path 객체로 변환 후 Path 객체로 변환
-			String decodedKey = URLDecoder.decode(url.getPath(),
-				StandardCharsets.UTF_8);// URI에서 경로 부분을 가져와 URL 디코딩을 통해 실제 키로 변환
-			return decodedKey.substring(1); // 경로 앞에 '/'가 있으므로 이를 제거한 뒤 반환
-		} catch (Exception exception) {
-			log.error(exception.getMessage(), exception);
-			throw new ApiException(ErrorStatus.INVALID_URL_FORMAT);
-		}
 	}
 
 	@Transactional
