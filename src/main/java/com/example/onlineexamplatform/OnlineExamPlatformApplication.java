1f1bd3b373ec65fea820beef4f9350d509ce7e89
HanGil <jhg9240@gmail.com>
2025-06-12 10:25:07 +0900

    feat: Scheduler 사용으로 Application 클래스에 @EnableScheduling 추가

--- src/main/java/com/example/onlineexamplatform/OnlineExamPlatformApplication.java
+++ src/main/java/com/example/onlineexamplatform/OnlineExamPlatformApplication.java
@@ -3,13 +3,15 @@ package com.example.onlineexamplatform;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
 public class OnlineExamPlatformApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(OnlineExamPlatformApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineExamPlatformApplication.class, args);
+	}
 
 }
