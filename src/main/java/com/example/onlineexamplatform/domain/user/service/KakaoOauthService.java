52fbfba2b2a582688ea439f0ad77815250c9967b
HanGil <jhg9240@gmail.com>
2025-07-02 20:00:59 +0900

    refactor: RedisSession 및 Cookie 발급 서비스 분리

--- src/main/java/com/example/onlineexamplatform/domain/user/service/KakaoOauthService.java
+++ src/main/java/com/example/onlineexamplatform/domain/user/service/KakaoOauthService.java
@@ -1,10 +1,6 @@
 package com.example.onlineexamplatform.domain.user.service;
 
-import java.time.Duration;
-import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
@@ -15,12 +11,9 @@ import com.example.onlineexamplatform.config.session.SessionUser;
 import com.example.onlineexamplatform.domain.user.dto.AuthLoginResponse;
 import com.example.onlineexamplatform.domain.user.dto.KakaoTokenResponse;
 import com.example.onlineexamplatform.domain.user.dto.KakaoUserInfoResponse;
-import com.example.onlineexamplatform.domain.user.entity.LoginProvider;
-import com.example.onlineexamplatform.domain.user.entity.Role;
 import com.example.onlineexamplatform.domain.user.entity.User;
 import com.example.onlineexamplatform.domain.user.repository.UserRepository;
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -30,11 +23,9 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 @Service
 public class KakaoOauthService {
-	private static final String SESSION_COOKIE_NAME = "SESSION";
-	private static final Duration SESSION_TTL = Duration.ofMinutes(15);
 	private final WebClient webClient;
 	private final UserRepository userRepository;
-	private final RedisTemplate<String, SessionUser> redisTemplate;
+	private final RedisService redisService;
 	@Value("${kakao.client_id}")
 	private String clientId;
 
@@ -72,13 +63,7 @@ public class KakaoOauthService {
 
 		// 카카오 유저 정보 유저테이블에 저장
 		User user = userRepository.findByVendorId(kakaoUserInfo.getId())
-			.orElseGet(() -> userRepository.save(User.builder()
-				.vendorId(kakaoUserInfo.getId())
-				.email(kakaoUserInfo.getKakaoAccount().getEmail())
-				.username(kakaoUserInfo.getProperties().getNickName())
-				.role(Role.USER)
-				.loginProvider(LoginProvider.KAKAO)
-				.build()));
+			.orElseGet(() -> userRepository.save(User.fromKakao(kakaoUserInfo)));
 
 		// 세션 객체 생성
 		SessionUser session = new SessionUser(
@@ -88,17 +73,8 @@ public class KakaoOauthService {
 			user.getLoginProvider()
 		);
 
-		// Redis 저장
-		String sessionId = UUID.randomUUID().toString();
-		String redisKey = SESSION_COOKIE_NAME + ":" + sessionId;
-		redisTemplate.opsForValue().set(redisKey, session, SESSION_TTL);
-
-		// 쿠키 발급
-		Cookie cookie = new Cookie(SESSION_COOKIE_NAME, sessionId);
-		cookie.setHttpOnly(true);
-		cookie.setPath("/");
-		cookie.setMaxAge((int)SESSION_TTL.getSeconds());
-		response.addCookie(cookie);
+		// Redis에 Session 저장 및 쿠키 생성
+		redisService.createRedisSession(session, response);
 
 		// 응답 DTO 생성
 		return AuthLoginResponse.of(user);
