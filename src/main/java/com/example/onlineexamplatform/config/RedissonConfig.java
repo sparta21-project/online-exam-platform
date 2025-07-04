08daa8cb389cff4825af5fc3e3fb90fbfa8cc347
SaltBr <snowgriff123@gmail.com>
2025-07-04 11:11:05 +0900

    fix: RedissonConfig value 수정

--- src/main/java/com/example/onlineexamplatform/config/RedissonConfig.java
+++ src/main/java/com/example/onlineexamplatform/config/RedissonConfig.java
@@ -10,10 +10,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RedissonConfig {
 
-    @Value("${redis.host}")
+    @Value("${spring.data.redis.host}")
     private String redisHost;
 
-    @Value("${redis.port}")
+    @Value("${spring.data.redis.port}")
     private int redisPort;
 
     @Bean(destroyMethod = "shutdown")
