bf6cce238ff765ee52871d9dc4ae31dff57e856b
SaltBr <snowgriff123@gmail.com>
2025-07-04 11:03:32 +0900

    fix: RedissonConfig 추가

--- src/main/java/com/example/onlineexamplatform/config/RedissonConfig.java
+++ src/main/java/com/example/onlineexamplatform/config/RedissonConfig.java
@@ -1,8 +1,33 @@
 package com.example.onlineexamplatform.config;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.IOException;
+@Configuration
+public class RedissonConfig {
 
+    @Value("${redis.host}")
+    private String redisHost;
 
+    @Value("${redis.port}")
+    private int redisPort;
+
+    @Bean(destroyMethod = "shutdown")
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://" + redisHost + ":" + redisPort)
+                .setConnectionMinimumIdleSize(2)
+                .setConnectionPoolSize(10)
+                .setIdleConnectionTimeout(10000)
+                .setConnectTimeout(10000)
+                .setTimeout(3000)
+                .setRetryAttempts(3)
+                .setRetryInterval(1500);
+        return Redisson.create(config);
+    }
+}
