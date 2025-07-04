bc9f6a11cfd85c8a171efa59d828868ee5e7dfe3
SaltBr <snowgriff123@gmail.com>
2025-07-04 12:20:31 +0900

    feat: redisson 설정 변경 및 application-prod.yml에 레디스 노드 추가

--- src/main/java/com/example/onlineexamplatform/config/RedissonConfig.java
+++ src/main/java/com/example/onlineexamplatform/config/RedissonConfig.java
@@ -1,5 +1,3 @@
-package com.example.onlineexamplatform.config;
-
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
@@ -10,24 +8,22 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RedissonConfig {
 
-    @Value("${spring.data.redis.host}")
-    private String redisHost;
-
-    @Value("${spring.data.redis.port}")
-    private int redisPort;
+    @Value("${REDIS_NODE_1}")
+    private String redisNode1;
 
     @Bean(destroyMethod = "shutdown")
     public RedissonClient redissonClient() {
         Config config = new Config();
-        config.useSingleServer()
-                .setAddress("redis://" + redisHost + ":" + redisPort)
-                .setConnectionMinimumIdleSize(2)
-                .setConnectionPoolSize(10)
+        config.useClusterServers()
+                .addNodeAddress("redis://" + redisNode1)
+                .setScanInterval(2000)
                 .setIdleConnectionTimeout(10000)
                 .setConnectTimeout(10000)
                 .setTimeout(3000)
                 .setRetryAttempts(3)
-                .setRetryInterval(1500);
+                .setRetryInterval(1500)
+                .setFailedSlaveReconnectionInterval(3000)
+                .setFailedSlaveCheckInterval(60000);
         return Redisson.create(config);
     }
 }
